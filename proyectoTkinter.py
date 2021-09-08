2a622ddf84e86ae65ec691315a9116a1be25eb73
Ismael Dominguez <ismaeldom11@gmail.com>
2021-09-08 18:41:51 -0500

    Se aumenta el tamaño de los encabezados y se crea un formulario.

--- proyectoTkinter.py
+++ proyectoTkinter.py
@@ -10,7 +10,7 @@ ventana.resizable(0,0)
 # Pantallas
 def home():
     # Montar Pantalla
-    home_label.config(fg="white", bg="black", font=("Arial", 30), padx=20, pady=20)
+    home_label.config(fg="white", bg="black", font=("Arial", 30), padx=210, pady=20)
     home_label.grid(row=0, column=0)
 
     # Ocultar otras pantallas
@@ -22,7 +22,7 @@ def home():
 
 def add():
     # Montar Pantalla
-    add_label.config(fg="white", bg="black", font=("Arial", 30), padx=20, pady=20)
+    add_label.config(fg="white", bg="black", font=("Arial", 30), padx=120, pady=20)
     add_label.grid(row=0, column=0)
 
     # Ocultar otras pantallas
@@ -34,7 +34,7 @@ def add():
 
 def info():
     # Montar Pantalla
-    info_label.config(fg="white", bg="black", font=("Arial", 30), padx=20, pady=20)
+    info_label.config(fg="white", bg="black", font=("Arial", 30), padx=150, pady=20)
     info_label.grid(row=0, column=0)
 
     data_label.grid(row=1, column=0)
@@ -45,11 +45,25 @@ def info():
 
     return True
 
+# Variables importantes
+name_data = StringVar()
+price_data = StringVar()
+
 # Definir campos de pantallas
 home_label = Label(ventana, text = "Inicio")
 add_label = Label(ventana, text = "Añadir Producto")
 data_label = Label(ventana, text="Creado por Ismael")
 info_label = Label(ventana, text = "Información")
+
+# Campos del formulario
+add_name_label = Label(ventana, text="Nombre del Producto")
+add_name_entry = Entry(ventana, textvariable=name_data)
+
+add_price_label = Label(ventana, text="Precio del Producto")
+add_price_entry = Entry(ventana, textvariable=price_data)
+
+add_description_label = Label(ventana, text="Descripción")
+add_description_entry = Text(ventana)
 
 # Carga Menu
 home()
